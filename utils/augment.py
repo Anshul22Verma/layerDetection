228ac750f9c3f96707359e20e973834fbceeb4db
Anshul22Verma <av.vermaans@gmail.com>
2025-02-03 15:50:45 -0500

    correcting augmentations got image stacking

--- utils/augment.py
+++ utils/augment.py
@@ -3,7 +3,7 @@ from torchvision import transforms
 
 def basic_classification_augmentation():
     return transforms.Compose([
-            transforms.Resize(756),
+            transforms.Resize((756, 756)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([transforms.GaussianBlur(kernel_size=3)], p=0.5),  # Strong blur
             transforms.ToTensor(),
@@ -14,7 +14,7 @@ class MoCoAugment:
     """ MoCo v2: Strong augmentations for query, weak for key. """
     def __init__(self):
         self.strong = transforms.Compose([
-            transforms.Resize(756),
+            transforms.Resize((756, 756)),
             transforms.RandomResizedCrop(224, scale=(0.08, 1.0)),  # Strong random crop
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
@@ -25,7 +25,7 @@ class MoCoAugment:
         ])
         
         self.weak = transforms.Compose([
-            transforms.Resize(756),
+            transforms.Resize((756, 756)),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -39,7 +39,7 @@ class SimCLRAugment:
     """ Strong augmentations as per the original SimCLR paper. """
     def __init__(self):
         self.transform = transforms.Compose([
-            transforms.Resize(756),
+            transforms.Resize((756, 756)),
             transforms.RandomResizedCrop(224, scale=(0.08, 1.0)),  # Large scale variation
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
@@ -57,7 +57,7 @@ class SimSiamAugment:
     """ SimSiam augmentations as per the paper. """
     def __init__(self):
         self.transform = transforms.Compose([
-            transforms.Resize(756),
+            transforms.Resize((756, 756)),
             transforms.RandomResizedCrop(224, scale=(0.2, 1.0)),  # Larger scale range
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
@@ -75,7 +75,7 @@ class BYOLAugment:
     """ BYOL augmentations as per the paper. """
     def __init__(self):
         self.transform = transforms.Compose([
-            transforms.Resize(756),
+            transforms.Resize((756, 756)),
             transforms.RandomResizedCrop(224, scale=(0.2, 1.0)),  # Strong augmentation
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)], p=0.8),
